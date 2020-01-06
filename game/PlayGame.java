36bdeeed3a743fd4174fa099a4af629e1599a5ca
sammytripp <samtripp5@gmail.com>
2020-01-06 12:54:47 -0500

    Randomize starting player

--- game/PlayGame.java
+++ game/PlayGame.java
@@ -1,11 +1,22 @@
+/**
+ * Provides the main method to begin execution of TicTacToe game.
+ *
+ */
 public class PlayGame {
 
     private static TicTacToeModel game;
     private static TicTacToeController controller;
 
     public static void main(String[] args){
-        // TODO randomize start player
-        game = new TicTacToeModel('X');
+
+        // Randomize starting player and initialize game board
+        if (((int) Math.random() % 2) == 0) {
+            game = new TicTacToeModel('X');
+        } else {
+            game = new TicTacToeModel('O');
+        }
+
+        // Initialize controller
         controller = new TicTacToeController(game);
     }
 }
